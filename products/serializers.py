f16060f055786d5a662bd3e75927d2f3bbd6129b
Samandar <samandarnayimjonov@gmail.com>
2025-03-17 12:12:47 +0500

    product serializer done

--- products/serializers.py
+++ products/serializers.py
@@ -1,1 +1,17 @@
 from rest_framework import serializers
+from .models import Product
+from comments.models import Comment
+
+
+class ProductCommentSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    content = serializers.CharField()
+
+class ProductSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    description = serializers.CharField()
+    price = serializers.DecimalField(max_digits=12, decimal_places=2)
+    comments = ProductCommentSerializer(many=True)
+
+
