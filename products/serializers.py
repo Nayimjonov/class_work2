8a6c4a7ae5ffc74a70c5ef2ca93c9c0eea8b2023
Samandar <samandarnayimjonov@gmail.com>
2025-03-17 12:16:20 +0500

    product views done and urls

--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-from .models import Product
-from comments.models import Comment
 
 
 class ProductCommentSerializer(serializers.Serializer):
@@ -12,6 +10,6 @@ class ProductSerializer(serializers.Serializer):
     name = serializers.CharField()
     description = serializers.CharField()
     price = serializers.DecimalField(max_digits=12, decimal_places=2)
-    comments = ProductCommentSerializer(many=True)
+    comments = ProductCommentSerializer(many=True, read_only=True)
 
 
